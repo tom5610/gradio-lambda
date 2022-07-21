90c8884235a532907fc2fee2700292ce3bc9da15
Neil Blake <neil0blake@gmail.com>
2022-07-22 00:33:36 +1200

    added comment for flagging

--- app/app.py
+++ app/app.py
@@ -4,6 +4,10 @@ from mangum import Mangum
 def greet(name):
     return "Hello " + name + "!!"
 
+# allow_flagging="never" is important 
+# for AWS lambda as the default directory is not writable
+# EFS will need to be setup for flagging to be 
+# useful
 demo = gr.Interface(fn=greet, inputs="text", outputs="text", allow_flagging="never")
 
 app, _, _ = demo.launch(prevent_thread_lock=True)
